fd5b424b3c006d6d559a1842bb971ebb2d0f5fab
r <reginaldbeakes@gmail.com>
2024-11-12 18:22:28 -0500

    init

---
+++ proxmox_template.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+apt update -y
+apt install libguestfs-tools -y
+
+CORES="${CORES:-1}"
+MEMORY="${MEMORY:-2048}"
+DISK_SIZE="${DISK_SIZE:-16G}"
+TEMPLATE_ID="${TEMPLATE_ID:-9001}"
+
+CLOUD_IMAGE_FILE="wget -qO- -P /var/lib/vz/template/iso/ $CLOUD_IMAGE_URL"
+
+virt-customize -a "$CLOUD_IMAGE_FILE" --install qemu-guest-agent
+
+qm create "$TEMPLATE_ID" --name "$TEMPLATE_NAME" --cores "$CORES" --memory "$MEMORY" --net0 virtio,bridge=vmbr0
+qm importdisk "$TEMPLATE_ID" "$CLOUD_IMAGE_FILE" local-lvm
+qm set "$TEMPLATE_ID" --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-"$TEMPLATE_ID"-disk-0
+qm set "$TEMPLATE_ID" --boot c --bootdisk scsi0
+qm set "$TEMPLATE_ID" --ide2 local-lvm:cloudinit
+qm set "$TEMPLATE_ID" --agent 1
+qm set "$TEMPLATE_ID" --machine q35
+qm set "$TEMPLATE_ID" --serial0 socker --cga serial0
+qm set "$TEMPLATE_ID" --ipconfig0 ip=dhcp
+qm resize "$TEMPLATE_ID" scsi0 "$DISK_SIZE"
+qm template "$TEMPLATE_ID"
