8aaba46cf1ff5a043340eac41e528d496fdfe3f9
r <reginaldbeakes@gmail.com>
2024-11-12 23:00:54 -0500

    bash updates

--- proxmox_template.sh
+++ proxmox_template.sh
@@ -30,9 +30,9 @@ echo Disk Size "$DISK_SIZE"
 echo Template ID "$TEMPLATE_ID"
 
 wget -O "$DOWNLOAD_FOLDER$CLOUD_IMAGE_FILE" $CLOUD_IMAGE_URL
-virt-customize -a "$CLOUD_IMAGE_FILE" --install qemu-guest-agent
+virt-customize -a "$DOWNLOAD_FOLDER$CLOUD_IMAGE_FILE" --install qemu-guest-agent
 qm create "$TEMPLATE_ID" --name "$TEMPLATE_NAME" --cores "$CORES" --memory "$MEMORY" --net0 virtio,bridge=vmbr0
-qm importdisk "$TEMPLATE_ID" "$DOWNLOAD_FOLDER""$CLOUD_IMAGE_FILE" local-lvm
+qm importdisk "$TEMPLATE_ID" "$DOWNLOAD_FOLDER$CLOUD_IMAGE_FILE" local-lvm
 qm set "$TEMPLATE_ID" --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-"$TEMPLATE_ID"-disk-0
 qm set "$TEMPLATE_ID" --boot c --bootdisk scsi0
 qm set "$TEMPLATE_ID" --ide2 local-lvm:cloudinit
