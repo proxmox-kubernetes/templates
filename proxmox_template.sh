ff98b48e95d6b4d0f7604a3bf2800eb19681992e
r <reginaldbeakes@gmail.com>
2024-11-12 22:55:23 -0500

    bash updates

--- proxmox_template.sh
+++ proxmox_template.sh
@@ -7,6 +7,7 @@ DISTRO="${DISTRO:-debian}"
 case $DISTRO in
   debian)
     CLOUD_IMAGE_URL="https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2"
+    CLOUD_IMAGE_FILE="debian-12-generic-amd64.qcow2"
     ;;
   *)
     echo "No Distro Set" >&2
@@ -21,16 +22,14 @@ TEMPLATE_ID="${TEMPLATE_ID:-9001}"
 
 echo Distro "$DISTRO"
 echo Cloud Image URL "$CLOUD_IMAGE_URL"
+echo Cloud Image File "$CLOUD_IMAGE_FILE"
 echo Core "$CORES"
 echo Memory "$MEMORY"
 echo Disk Size "$DISK_SIZE"
 echo Template ID "$TEMPLATE_ID"
 
-CLOUD_IMAGE_FILE="$(wget -qPO- /var/lib/vz/template/iso/ $CLOUD_IMAGE_URL)"
-echo Cloud Image File "$CLOUD_IMAGE_FILE"
-
+wget -O /var/lib/vz/template/iso/$CLOUD_IMAGE_FILE $CLOUD_IMAGE_URL
 virt-customize -a "$CLOUD_IMAGE_FILE" --install qemu-guest-agent
-
 qm create "$TEMPLATE_ID" --name "$TEMPLATE_NAME" --cores "$CORES" --memory "$MEMORY" --net0 virtio,bridge=vmbr0
 qm importdisk "$TEMPLATE_ID" "$CLOUD_IMAGE_FILE" local-lvm
 qm set "$TEMPLATE_ID" --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-"$TEMPLATE_ID"-disk-0
