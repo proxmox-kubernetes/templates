737bb062743e7213dc98609fd88891a2883ef927
r <reginaldbeakes@gmail.com>
2024-11-12 23:13:53 -0500

    bash updates

--- proxmox_template.sh
+++ proxmox_template.sh
@@ -15,7 +15,7 @@ case $DISTRO in
     ;;
 esac
 
-DOWNLOAD_FOLDER="${DOWNLOAD_FOLDER:-/var/lib/vz/tamplate/iso/}"
+CLOUD_IMAGE_FILE="${DOWNLOAD_FOLDER:-/var/lib/va/template/iso/}$CLOUD_IMAGE_FILE"
 CORES="${CORES:-1}"
 MEMORY="${MEMORY:-2048}"
 DISK_SIZE="${DISK_SIZE:-16G}"
@@ -29,16 +29,16 @@ echo Memory "$MEMORY"
 echo Disk Size "$DISK_SIZE"
 echo Template ID "$TEMPLATE_ID"
 
-wget -O "$DOWNLOAD_FOLDER$CLOUD_IMAGE_FILE" $CLOUD_IMAGE_URL
-virt-customize -a "$DOWNLOAD_FOLDER$CLOUD_IMAGE_FILE" --install qemu-guest-agent
+wget -O "$CLOUD_IMAGE_FILE" $CLOUD_IMAGE_URL
+virt-customize -a "$CLOUD_IMAGE_FILE" --install qemu-guest-agent
 qm create "$TEMPLATE_ID" --name "$TEMPLATE_NAME" --cores "$CORES" --memory "$MEMORY" --net0 virtio,bridge=vmbr0
-qm importdisk "$TEMPLATE_ID" "$DOWNLOAD_FOLDER$CLOUD_IMAGE_FILE" local-lvm
+qm importdisk "$TEMPLATE_ID" "$CLOUD_IMAGE_FILE" local-lvm
 qm set "$TEMPLATE_ID" --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-"$TEMPLATE_ID"-disk-0
 qm set "$TEMPLATE_ID" --boot c --bootdisk scsi0
 qm set "$TEMPLATE_ID" --ide2 local-lvm:cloudinit
 qm set "$TEMPLATE_ID" --agent 1
 qm set "$TEMPLATE_ID" --machine q35
-qm set "$TEMPLATE_ID" --serial0 socker --cga serial0
+qm set "$TEMPLATE_ID" --serial0 socker --vga serial0
 qm set "$TEMPLATE_ID" --ipconfig0 ip=dhcp
 qm resize "$TEMPLATE_ID" scsi0 "$DISK_SIZE"
 qm template "$TEMPLATE_ID"
