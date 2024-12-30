d833f0bc34321c12ed5171ed667e3edb6eadb9a0
Reggles <reginaldbeakes@gmail.com>
2024-12-29 22:57:12 -0500

    adding user-data.yaml

--- proxmox_template.sh
+++ proxmox_template.sh
@@ -12,7 +12,7 @@ USER_DATA=/var/lib/vz/snippets/user-data.yaml
 case $DISTRO in
 debian)
   CLOUD_IMAGE_URL="https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2"
-  CLOUD_IMAGE_FILE="debian-12-generic-amd64.qcow2"
+  CLOUD_IMAGE_FILE="/tmp/debian-12-generic-amd64.qcow2"
   TEMPLATE_NAME="debian-12-cloud"
   ;;
 *)
@@ -87,7 +87,7 @@ qm set "$VMID" --ipconfig0 ip=dhcp
 # qm importdisk "$VMID" "$CLOUD_IMAGE_FILE" local-lvm
 # qm set "$VMID" --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-"$VMID"-disk-0
 qm set "$VMID" --scsihw virtio-scsi-pci
-qm set "$VMID" --scsi0 local-lvm:vm-"$VMID"-disk-0,import-from=./"$CLOUD_IMAGE_FILE",discard=on,ssd=1
+qm set "$VMID" --scsi0 local-lvm:0,import-from="$CLOUD_IMAGE_FILE",discard=on,ssd=1
 # qm set "$VMID" --scsi0 local-lvm:vm-"$VMID"-disk-0
 qm set "$VMID" --ide2 local-lvm:cloudinit
 qm set "$VMID" --boot order=scsi0
@@ -103,4 +103,4 @@ qm set "$VMID" --cicustom "user=local:snippets/user-data.yaml"
 qm template "$VMID"
 
 # Clean up
-# rm $CLOUD_IMAGE_FILE
+rm $CLOUD_IMAGE_FILE
